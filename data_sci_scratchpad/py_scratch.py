7aa888064cbdf7edc1c458637a27fafea0e0e09c
data-alch3m1st <d4t4.4lch3my+g1thub@gmail.com>
2025-10-02 11:40:54 -0400

    Create py_scratch.py

---
+++ data_sci_scratchpad/py_scratch.py
@@ -0,0 +1,11 @@
+# py_scratch.py #
+
+    """
+    This IS NOT a standalone script; it is a scratchpad for Python code snippets, which I have written, 'borrowed' and duct-taped together during my time as a data-scientist and fraud fighter, while battling the evil scoundrels and villains whose MO is cyber-enabled and cyber-dependant fraud. (As well as through some empassioned tinkering and bamboozlery among so-called 'toy' datasets).
+    
+    Feel free to use, borrow, take, steal, adapt, modify, and repurpose any of the code snippets herein, as you see fit. If you do so, I would appreciate a shout-out or a mention in your acknowledgements. Happy coding!
+    
+    P.S. "Choose not thy thugg-lyfe and/or or bedevilment, but per chance a path less eville." - Me
+    """
+    
+# Misc pandas 🐼🐼🐼 to live by # 
