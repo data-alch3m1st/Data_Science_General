1fdb47e47ff2b182781e66a772d3fa4b30808abc
data-alch3m1st <d4t4.4lch3my+g1thub@gmail.com>
2026-01-13 08:56:58 -0500

    Update py_scratch.py

--- data_sci_scratchpad/py_scratch.py
+++ data_sci_scratchpad/py_scratch.py
@@ -317,7 +317,21 @@ import json
 # Set the folder PATH where the JSON files are located:
 folder_path = './input/json_api_data/'
 
+# Initialize an empty list to store individual dataframes:
+dataframes = []
 
+# Loop through each file in the folder:
+for filename in os.listdir(folder_path):
+    if filename.endswith('.json'):
+        file_path = os.path.join(folder_path, filename)
+        
+        # Read the JSON file into a dataframe:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            df = pd.json_normalize(data)  # Normalize semi-structured JSON data into a flat table
+            
+            # Append the dataframe to the list:
+            dataframes.append(df)   
 
 # ----------------------------------------------------------------------------------------------- #
 # ----------------------------------------------------------------------------------------------- #
